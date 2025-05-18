bc8a3a6286d9b23ea55a5edec74443fa52cec912
Admin <areg.aghabekyan.y@tumo.org>
2025-05-18 12:17:32 +0400

    Somthing new

--- Chess-app/Program.cs
+++ Chess-app/Program.cs
@@ -1,28 +1,26 @@
-﻿string[,] str = new string[8, 8];
+﻿string[,] str = new string[8, 8];//TODO: Board class in cs file
 
-Console.WriteLine("Enter position=");
+Console.Write("Enter position=");//TODO: GameIO class in cs file 
 string position1 = Console.ReadLine();
 PlaceMarker(position1);
 DrawBoard();
 
-Console.WriteLine("Enter position=");
+Console.Write("Enter position=");
 string position2 = Console.ReadLine();
 PlaceMarker(position2);
 DrawBoard();
 
-RockMove();
+//RockMove();
 DrawBoard();
-Console.WriteLine(2);
-
+KnightMoveCount();
+Console.ReadLine();
 void DrawBoard()
 {
-    for (int i = 0; i < str.GetLength(0); i++)
+    for (int i = 0; i < 8; i++)
     {
-
         Console.Write($" {8 - i} ");
-        for (int j = 0; j < str.GetLength(1); j++)
+        for (int j = 0; j < 8; j++)
         {
-
             if ((i + j) % 2 == 0)
             {
                 Console.BackgroundColor = ConsoleColor.White;
@@ -31,17 +29,12 @@ void DrawBoard()
             {
                 Console.BackgroundColor = ConsoleColor.Black;
             }
-
-
             Console.ForegroundColor = ConsoleColor.Green;
-
             Console.Write($" {str[i, j] ?? " "} ");
         }
         Console.ResetColor();
         Console.WriteLine();
     }
-
-
     Console.Write("   ");
     for (char c = 'A'; c <= 'H'; c++)
     {
@@ -50,23 +43,25 @@ void DrawBoard()
     Console.WriteLine();
 }
 
-void PlaceMarker(string pos)
+void PlaceMarker(string pos)//TODO: Board class in cs file
 {
-    if (pos.Length != 2) return;
+    if (pos.Length != 2) 
+        return;   //TODO: throw new exaption
 
     int col = char.ToUpper(pos[0]) - 'A';
 
     int row = 8 - (pos[1] - '0');
 
-    if (row < 0 || row > 7 || col < 0 || col > 7) return;
+    if (row < 0 || row > 7 || col < 0 || col > 7) 
+        return;//TODO: throw new exaption
 
-    str[row, col] = "X";
+    str[row, col] = "X"; 
 }
 
 void RockMove()
 {
     int x1 = char.ToUpper(position1[0]) - 'A';
-    int y1 = 8 - (position1[1] - '0');
+    int y1 = 8 - (position1[1] - '0');       //TODO: Coordinate struct
 
     int x2 = char.ToUpper(position2[0]) - 'A';
     int y2 = 8 - (position2[1] - '0');
@@ -107,4 +102,54 @@ void RockMove()
 
         str[y2, x2] = "X";
     }
+}
+void KnightMoveCount()
+{
+    int x1 = char.ToUpper(position1[0]) - 'A';
+    int y1 = 8 - (position1[1] - '0');
+
+    int x2 = char.ToUpper(position2[0]) - 'A';
+    int y2 = 8 - (position2[1] - '0');
+
+    int[,] memo = new int[8, 8];   //TODO:boolean 2d matrix 
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            memo[i, j] = -1;
+
+    int steps = MinKnightSteps(x1, y1, x2, y2, memo);
+    Console.WriteLine($"Minimum knight moves from {position1} to {position2}:{steps}");
+
+}
+int MinKnightSteps(int x, int y, int targetX, int targetY, int[,] memo)
+{
+    if (x < 0 || x >= 8 || y < 0 || y >= 8)
+        return int.MaxValue;
+
+    if (x == targetX && y == targetY)
+        return 0;
+
+    if (memo[x, y] != -1)
+        return memo[x, y];
+
+    memo[x, y] = int.MaxValue - 1; 
+
+    int[] dRow = { -2, -2, -1, 1, 2, 2, 1, -1 };
+    int[] dCol = { -1, 1, 2, 2, 1, -1, -2, -2 };
+
+    int min = int.MaxValue;
+
+    for (int i = 0; i < 8; i++)
+    {
+        int nextX = x + dRow[i];
+        int nextY = y + dCol[i];
+
+        int steps = MinKnightSteps(nextX, nextY, targetX, targetY, memo);
+
+        if (steps != int.MaxValue)
+        {
+            min = Math.Min(min, steps + 1);
+        }
+    }
+    memo[x, y] = min; 
+    return min;
 }
--- Chess-app/obj/Release/net8.0/Chess-app.AssemblyInfo.cs
+++ Chess-app/obj/Release/net8.0/Chess-app.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chess-app")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e19182d033c3267a550751598707f18bfed16c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chess-app")]
